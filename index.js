a85c292e63ac77754c6aaa608fd050b407b6126b
Gireeshkumar Ediga <egireeshkumar99@gmail.com>
2022-03-19 10:11:01 +0530

    code added

---
+++ index.js
@@ -0,0 +1,14 @@
+[
+    {
+        "text" : "textone",
+        "dateTime" : "2020-07-10 15:00:00.000"
+    },
+    {
+        "text" : "textTwo",
+        "dateTime" : "2020-07-15 14:00:00.000"
+    },
+    {
+        "text" : "textFinal",
+        "dateTime" : "2020-07-15 15:00:00.000"
+    }
+]
